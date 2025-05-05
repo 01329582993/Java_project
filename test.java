05c35f3525e71d3f19fb4aa78e130037d70d8370
LIMASSOU HAMAN BELLO <149891433+01329582993@users.noreply.github.com>
2025-05-05 10:58:08 +0600

    Add files via upload
    Java first program

---
+++ test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class test{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name :");
+
+        String name  = scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+
+        int age = scanner.nextInt();
+        
+        System.out.println("Dear ",name);
+    }
+}
